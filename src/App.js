98fd695179737c72967a795c0cffa288e1d60e7d
31000s <burakyabgu@gmail.com>
2023-01-28 01:09:13 +0300

    Todo eklemek için gerekli ortam sağlandı.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-function App() {
-return(
-    <div className="App">
-      <h1>TODO LIST</h1>
+import { useState } from "react";
 
-      <form>
-        <h3>İtem Ekle: <input type="text" placeholder="A New Item"/></h3>
-        <button>Oluştur</button>
-      </form>
-    </div>
-  );
-}
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem(){
+    
+    const item = {
+      id: Math.floor(Math.random()*1000),
+      value: newItem
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
+  }
+
+
+  return(
+      <div className="App">
+        <h1>TODO LIST</h1>
+
+        <form onSubmit={(e)=> {
+          e.preventDefault();
+          addItem();
+        }}>
+          <h3>İtem Ekle: <input type="text" placeholder="A New Item"/></h3>
+          <button>Oluştur</button>
+        </form>
+      </div>
+    );
+  }
 
 export default App;
