dc893522a83e828cbca2e78884df5b537041011d
31000s <burakyabgu@gmail.com>
2023-01-28 01:03:01 +0300

    Form Stilleri Tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
-
 function App() {
-  return (
-    <>
-    </>
+return(
+    <div className="App">
+      <h1>TODO LIST</h1>
+
+      <form>
+        <h3>İtem Ekle: <input type="text" placeholder="A New Item"/></h3>
+        <button>Oluştur</button>
+      </form>
+    </div>
   );
 }
 
